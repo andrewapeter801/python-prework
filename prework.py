b853ea73c1fe9217608fed2216ad4b51df1810e9
andrewapeter801 <144055669+andrewapeter801@users.noreply.github.com>
2023-09-07 09:14:14 -0600

    first commit

---
+++ prework.py
@@ -0,0 +1,7 @@
+famousQuote = "Life is about growth!"
+first = "Andrew"
+last = "Peterson"
+
+message = (first) + " " + (last) + " " + "once said," + " " + (famousQuote)
+#message = f'{(first) + (last) + "once said," + (famousQuote)}'
+print(message)
